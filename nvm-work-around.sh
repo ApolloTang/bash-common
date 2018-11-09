65e5f3f81ab4d23f5700c357034b0d72c32a4143
Apollo Tang <tang.apollo@gmail.com>
2018-11-09 15:11:21 -0500

    Update nvm-work-around.sh
    change to load nvm manually instead when running node related command

--- nvm-work-around.sh
+++ nvm-work-around.sh
@@ -1,30 +1,32 @@
 # https://github.com/creationix/nvm/issues/1277#issuecomment-394186011
 # http://broken-by.me/lazy-load-nvm/
+# https://www.growingwiththeweb.com/
 
+export NVM_DIR=~/.nvm
 
-nvm_load() {
-    unset -f nvm
-    export NVM_DIR=~/.nvm
+nvmload() {
+    # unset -f nvm
+    # export NVM_DIR=~/.nvm
     [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
     [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-    nvm "$@"
+    # nvm "$@"
 }
 
-node_load() {
-    unset -f node
-    export NVM_DIR=~/.nvm
-    [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
-    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-    node "$@"
-}
-
-npm_load() {
-    unset -f npm
-    export NVM_DIR=~/.nvm
-    [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
-    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-    npm "$@"
-}
+# node_load() {
+#     unset -f node
+#     export NVM_DIR=~/.nvm
+#     [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+#     [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+#     node "$@"
+# }
+#
+# npm_load() {
+#     unset -f npm
+#     export NVM_DIR=~/.nvm
+#     [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+#     [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+#     npm "$@"
+# }
 
 # The above will replace the default loading nvm script
 # I use it for nvm version 0.33.11 on work computer
